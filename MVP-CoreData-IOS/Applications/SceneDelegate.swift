cf551861fccfb6fa2f813d0a11fc1997a4ecb0e2
Egor Ivanov <101597786+Ozondo@users.noreply.github.com>
2024-01-23 13:36:01 +0300

    tabbar

--- MVP-CoreData-IOS/Applications/SceneDelegate.swift
+++ MVP-CoreData-IOS/Applications/SceneDelegate.swift
@@ -19,7 +19,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let scene = (scene as? UIWindowScene) else { return }
         
         window = UIWindow(windowScene: scene)
-        window?.rootViewController = OnePageButtonBuilder().build()
+        window?.rootViewController = TabBarController.createTabBar()
         window?.makeKeyAndVisible()
     }
 
